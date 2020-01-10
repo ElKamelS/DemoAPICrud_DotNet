e2b433080145d7160c427d9673512eee4a64bb85
Souhail El Kamel <souhail_elkamel@hotmail.com>
2020-01-10 17:53:03 +0100

    Connection to database configured

--- DemoAPICrud/Startup.cs
+++ DemoAPICrud/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+
 
 namespace DemoAPICrud
 {
@@ -25,6 +21,11 @@ namespace DemoAPICrud
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Spark_ErpContext>(opt =>
+               opt.UseSqlServer(Configuration["ConnectionStrings:Spark_ErpContext"]),ServiceLifetime.Transient
+               
+               );
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
